660e97c4c69b5b52c7a8bf17de5625ecb966bf5f
dtsatyam <dt.satyam>
2024-04-19 18:13:49 +0530

    Data ingestion pipeline added

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # US-VisaApprovalPrediction
+
+```bash
+conda create -p visa python==3.8 -y
+```
+
+```bash
+conda activate visa
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+## Workflow
+1. constant
+2. config_entity
+3. artifact_entity
+4. component
+5. pipeline
+6. app.py
+
+
+Run below command for setting mongodb url as environment variable
+```bash
+export MONGODB_URL = 'mongodb+srv://<username>:<password>@.....'
+```
+
--- demo.py
+++ demo.py
@@ -1,9 +1,20 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
 
-try:
-    a=1/'Satya'
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys) from e
+# try:
+#     a=1/'Satya'
+# except Exception as e:
+#     logging.info(e)
+#     raise USvisaException(e, sys) from e
+
+
+# from us_visa.constants import DATABASE_NAME, COLLECTION_NAME
+# print('Database name is:', DATABASE_NAME)
+# print('Collection name is:', COLLECTION_NAME)
+
+
+from us_visa.pipeline.training_pipeline import TrainPipeline
+pipeline = TrainPipeline()
+
+pipeline.run_pipeline()
